2500a4cd43cfafd21393f4681cb604b1a1ec69a3
bilzhang <macross.f@qq.com>
2022-08-22 20:20:02 +0800

    三线性插值

--- RayTrace/RayTrace/perlin.h
+++ RayTrace/RayTrace/perlin.h
@@ -4,9 +4,11 @@
 
 #ifndef RAY_TRACER_PERLIN_H
 #define RAY_TRACER_PERLIN_H
+
 #include "rtweekend.h"
 
 #include "vec3.h"
+
 class perlin {
 public:
     perlin() {
@@ -20,7 +22,7 @@ public:
         perm_z = perlin_generate_perm();
     }
 
-    ~perlin(){
+    ~perlin() {
         delete[] randfloat;
         delete[] perm_x;
         delete[] perm_y;
@@ -28,14 +30,48 @@ public:
     }
 
     double noise(const point3 &p) const {
-        auto i = static_cast<int>(4*p.x()) & 255;
-        auto j = static_cast<int>(4*p.y()) & 255;
-        auto k = static_cast<int>(4*p.z()) & 255;
-        return randfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        //≤Ó÷µ
+        auto u = p.x() - floor(p.x());
+        auto v = p.y() - floor(p.y());
+        auto w = p.z() - floor(p.z());
+
+        //
+        auto i = static_cast<int>(floor(p.x()));
+        auto j = static_cast<int>(floor(p.y()));
+        auto k = static_cast<int>(floor(p.z()));
+        double c[2][2][2];
+
+        for (int di = 0; di < 2; ++di) {
+            for (int dj = 0; dj < 2; ++dj) {
+                for (int dk = 0; dk < 2; ++dk) {
+                    c[di][dj][dk] = randfloat[
+                            perm_x[(i + di) & 255] ^
+                            perm_y[(j + dj) & 255] ^
+                            perm_z[(k + dk) & 255]
+                    ];
+                }
+            }
+        }
+        return trilinear_interp(c, u, v, w);
     }
 
-    static int* perlin_generate_perm() {
-        auto p = new int [point_count];
+    static double trilinear_interp(double c[2][2][2], double u, double v, double w) {
+        auto accum = 0.0;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                for (int k = 0; k < 2; ++k) {
+                    accum += (i * u + (1 - i) * (1 - u)) *
+                            (j * v + (1 - j) * (1 - v)) *
+                            (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+                }
+            }
+        }
+        return accum;
+    }
+
+
+    static int *perlin_generate_perm() {
+        auto p = new int[point_count];
         for (int i = 0; i < point_count; ++i) {
             p[i] = i;
         }
@@ -43,7 +79,7 @@ public:
         return p;
     }
 
-    static void permute(int* p, int n) {
+    static void permute(int *p, int n) {
         for (int i = n - 1; i > 0; i--) {
             int target = random_int(0, i);
             int tmp = p[i];
@@ -55,7 +91,7 @@ public:
 
 public:
     static const int point_count = 256;
-    double* randfloat;
+    double *randfloat;
     int *perm_x, *perm_y, *perm_z;
 };
 
